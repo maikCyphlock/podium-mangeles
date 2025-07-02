dfd5c3bb4b5452ccfd59093021b784d4c737809f
maikCyphlock <maikolaguilar656@gmail.com>
2025-07-02 18:01:31 -0400

    Se añaden enlaces a las imágenes de patrocinadores en el componente MarqueeSponsors, permitiendo que los usuarios accedan a los perfiles de Instagram de los patrocinadores al hacer clic en las imágenes. Esto mejora la interactividad y visibilidad de los patrocinadores en la página.

--- src/components/reactbits/MarqueeSponsors.tsx
+++ src/components/reactbits/MarqueeSponsors.tsx
@@ -3,6 +3,8 @@ export default function Sponsor() {
     <div className="w-full flex flex-col  mt-12 items-center justify-center py-6 bg-white/80 border-t border-b border-gray-100">
       <span className="text-sm font-bold text-gray-500 ">Patrocinado por:</span>
       <div className="flex gap-8 flex-wrap items-center justify-center">
+     
+      <a href="https://www.instagram.com/apssanrafael/" target="_blank">
       <img
         src="/patro-1.png"
         alt="Patrocinador"
@@ -11,6 +13,8 @@ export default function Sponsor() {
         className="rounded-xl aspect-video h-[200px]  bg-white"
         style={{ objectFit: "contain" }}
       />
+      </a>
+        <a href="https://www.instagram.com/agui_dom/" target="_blank">
         <img
         src="/aguidom.png"
         alt="Patrocinador"
@@ -19,6 +23,7 @@ export default function Sponsor() {
         className="rounded-xl aspect-video h-[130px]   bg-white"
         style={{ objectFit: "contain" }}
       />
+        </a>
       </div>
     </div>
   );
