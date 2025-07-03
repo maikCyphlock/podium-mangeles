45be9933d29bc3ef18216ee64da970b419d2da4a
maikCyphlock <maikolaguilar656@gmail.com>
2025-07-03 10:48:42 -0400

    Se realizan ajustes en el diseño de la página de inicio, aumentando los márgenes y el espaciado entre elementos para mejorar la presentación visual. Se modifica el encabezado del evento para resaltar el título y se actualizan los estilos de los bloques de información clave y actividades, optimizando la experiencia del usuario. Además, se corrige el texto del botón de inscripción para incluir la tilde en "INSCRÍBETE".

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,8 @@ export default function Home() {
   }, []);
 
   return (
-    <div id="landing-root" className="relative min-h-screen flex flex-col items-center justify-center bg-white overflow-x-hidden opacity-0 transition-opacity duration-300">
+    <div id="landing-root" className="relative min-h-screen flex flex-col items-center justify-center bg-white overflow-x-hidden opacity-0 transition-opacity duration-300  ">
+     
       {/* Aurora decorativa arriba, sutil */}
       <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-2 w-[120vw] h-[320px] pointer-events-none z-0 opacity-60" style={{ filter: 'blur(2px)' }}>
         <Aurora
@@ -60,9 +61,9 @@ export default function Home() {
           speed={0.5}
         />
       </div>
-      <main className="relative mt-12 z-10 w-full flex flex-col items-center justify-center">
-        <header className="flex flex-col items-center mb-8 w-full max-w-2xl">
-          <div ref={imageRef}>
+      <main className="relative mt-20 z-10 w-full flex flex-col items-center justify-center gap-12">
+        <header className="flex flex-col items-center mb-12 w-full max-w-2xl gap-4">
+          <div ref={imageRef} className="mb-8">
             <Image
               src="/mis-angeles.png"
               alt="Logo Farmacia Mis Ángeles"
@@ -74,9 +75,15 @@ export default function Home() {
           </div>
           <h1
             ref={titleRef}
-            className="font-title text-3xl sm:text-4xl md:text-5xl font-extrabold text-gray-800 mb-2 text-center drop-shadow-sm px-2"
+            className="font-title text-xl sm:text-3xl md:text-4xl font-semibold text-zinc-500 mb-2 mt-4 text-center tracking-wide uppercase relative"
           >
-            EVENTO DEPORTIVO "ÁNGELES CAMPEONES"
+            EVENTO DEPORTIVO
+            <span
+              className="block text-4xl sm:text-5xl md:text-6xl font-extrabold text-zinc-700  drop-shadow-lg tracking-tight leading-tight mt-2 mb-4"
+            >
+              "ÁNGELES CAMPEONES"
+            </span>
+            
           </h1>
           <p
             className="text-base sm:text-lg md:text-xl text-gray-700 text-center max-w-md sm:max-w-xl mx-auto px-2 heading-text font-medium mb-4"
@@ -85,37 +92,37 @@ export default function Home() {
           </p>
         </header>
         {/* Bloque de información clave */}
-        <section className="w-full max-w-2xl grid grid-cols-1 sm:grid-cols-2 gap-4 mb-8 p-2">
-          <div className="flex items-center gap-3 bg-zinc-50 rounded-xl p-4 shadow activity-item">
+        <section className="w-full max-w-2xl grid grid-cols-1 sm:grid-cols-2 gap-8 mb-12 p-2">
+          <div className="flex items-center gap-4 bg-zinc-50 rounded-xl p-6 shadow activity-item">
             <span className="text-2xl">
-            <img src="calendar.png" className="w-14" alt="" />
+              <img src="/calendar.png" className="w-14" alt="" />
             </span>
             <div>
               <div className="font-bold text-gray-800">Sábado 26 de julio</div>
               <div className="text-gray-600 text-sm">Desde las 8:30 am</div>
             </div>
           </div>
-          <div className="flex items-center gap-3 bg-zinc-50 rounded-xl p-4 shadow activity-item">
+          <div className="flex items-center gap-4 bg-zinc-50 rounded-xl p-6 shadow activity-item">
             <span className="text-2xl">
-            <img src="map.png" className="w-14" alt="" />
+              <img src="/map.png" className="w-14" alt="" />
             </span>
             <div>
               <div className="font-bold text-gray-800">Estadio José Antonio Páez</div>
               <div className="text-gray-600 text-sm">Pista de atletismo, Araure</div>
             </div>
           </div>
-          <div className="flex items-center gap-3 bg-zinc-50 rounded-xl p-4 shadow activity-item">
+          <div className="flex items-center gap-4 bg-zinc-50 rounded-xl p-6 shadow activity-item">
             <span className="text-2xl">
-              <img src="toddler.png" className="w-14" alt="" />
+              <img src="/toddler.png" className="w-14" alt="" />
             </span>
             <div>
               <div className="font-bold text-gray-800">Niños y niñas</div>
               <div className="text-gray-600 text-sm">De 3 a 12 años</div>
             </div>
           </div>
-          <div className="flex items-center gap-3 bg-zinc-50 rounded-xl p-4 shadow activity-item">
+          <div className="flex items-center gap-4 bg-zinc-50 rounded-xl p-6 shadow activity-item">
             <span className="text-2xl">
-            <img src="cash.png" className="w-14" alt="" />
+              <img src="/cash.png" className="w-14" alt="" />
             </span>
             <div>
               <div className="font-bold text-green-700">TOTALMENTE GRATIS</div>
@@ -123,25 +130,25 @@ export default function Home() {
           </div>
         </section>
         {/* Bloque de actividades */}
-        <section className="w-full max-w-2xl flex flex-col items-center mb-8">
-          <p className="text-base sm:text-lg text-gray-700 text-center mb-4 p-2">
-          Vive Una jornada llena de deportes, diversion y alegría.
-          Tendremos un montón de <strong>sorpresas</strong>, juegos y actividades para que cada niño se sienta un <strong>verdadero campeón</strong>:
+        <section className="w-full max-w-2xl flex flex-col items-center mb-12">
+          <p className="text-base sm:text-lg text-gray-700 text-center mb-8 p-2">
+            Vive Una jornada llena de deportes, diversión y alegría.
+            Tendremos un montón de <strong>sorpresas</strong>, juegos y actividades para que cada niño se sienta un <strong>verdadero campeón</strong>:
           </p>
-          <div ref={activitiesRef} className="w-full flex flex-col gap-5 sm:gap-6 md:flex-row md:gap-6 justify-center mb-2 px-2">
-            <div className="flex-1 flex flex-row md:flex-col items-center justify-center gap-3 md:gap-0 activity-item bg-white/90 rounded-2xl py-5 md:py-8 shadow-lg border border-gray-200 transition-all relative overflow-hidden">
+          <div ref={activitiesRef} className="w-full flex flex-col gap-8 sm:gap-10 md:flex-row md:gap-10 justify-center mb-2 px-2">
+            <div className="flex-1 flex flex-row md:flex-col items-center justify-center gap-4 md:gap-0 activity-item bg-white/90 rounded-2xl py-8 md:py-12 shadow-lg border border-gray-200 transition-all relative overflow-hidden">
               <span className="mb-0 md:mb-2">
                 <img src="/soccer.png" alt="Fútbol" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 z-10 relative" />
               </span>
               <span className="font-bold text-gray-800 text-lg sm:text-xl md:text-2xl z-10">Fútbol</span>
             </div>
-            <div className="flex-1 flex flex-row md:flex-col items-center justify-center gap-3 md:gap-0 activity-item bg-white/90 rounded-2xl py-5 md:py-8 shadow-lg border border-gray-200 transition-all relative overflow-hidden">
+            <div className="flex-1 flex flex-row md:flex-col items-center justify-center gap-4 md:gap-0 activity-item bg-white/90 rounded-2xl py-8 md:py-12 shadow-lg border border-gray-200 transition-all relative overflow-hidden">
               <span className="mb-0 md:mb-2">
                 <img src="/yimcana.png" alt="Yincana" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 z-10 relative" />
               </span>
               <span className="font-bold text-gray-800 text-lg sm:text-xl md:text-2xl z-10">Yincanas</span>
             </div>
-            <div className="flex-1 flex flex-row md:flex-col items-center justify-center gap-3 md:gap-0 activity-item bg-white/90 rounded-2xl py-5 md:py-8 shadow-lg border border-gray-200 transition-all relative overflow-hidden">
+            <div className="flex-1 flex flex-row md:flex-col items-center justify-center gap-4 md:gap-0 activity-item bg-white/90 rounded-2xl py-8 md:py-12 shadow-lg border border-gray-200 transition-all relative overflow-hidden">
               <span className="mb-0 md:mb-2">
                 <img src="/running.png" alt="Atletismo" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 z-10 relative" />
               </span>
@@ -149,17 +156,17 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <Link href="/event" className="w-full flex justify-center max-w-xs">
-          <RainbowButton className="text-lg font-bold hover:scale-110" >
-            ¡INSCRIBETE YA!
+        <Link href="/event" className="w-full flex justify-center max-w-xs mb-8">
+          <RainbowButton className="text-lg font-bold hover:scale-110 py-4 px-8" >
+            ¡INSCRÍBETE YA!
           </RainbowButton>
         </Link>
         <Sponsor />
-        <footer className="mt-12 sm:mt-16 w-full px-2 py-6 bg-white border-t border-gray-200 text-center text-xs sm:text-sm text-gray-500 shadow-none bg-zinc-900 text-zinc-100">
+        <footer className="mt-20 sm:mt-24 w-full px-2 py-8 bg-white border-t border-gray-200 text-center text-xs sm:text-sm text-gray-500 shadow-none bg-zinc-900 text-zinc-100">
           <div>
             © {new Date().getFullYear()} Farmacia Mis Ángeles
           </div>
-          <div className="mt-1 flex  items-center justify-center gap-1">
+          <div className="mt-2 flex items-center justify-center gap-2">
             <span>
               Hecho con mucho amor
             </span>
